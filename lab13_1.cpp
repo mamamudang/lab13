13490fd8d755ca456df3ca6d57b17c7dd2b0c5a1
mamamudang <humlake7.66@gmail.com>
2024-12-27 18:22:12 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,23 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0, sum_sq = 0, product = 1, harmonic_sum = 0;
+    double max = A[0], min = A[0];
+    for (int i = 0; i < N;i++){
+        sum += A[i];
+        sum_sq += A[i] * A[i];
+        product *= A[i];
+        harmonic_sum += 1.0 / A[i];
+        if (A[i] > max) max = A[i];
+        if (A[i] < min) min = A[i];
+    }
+    B[0] = sum / N;
+    B[1] = sqrt((sum_sq / N) - (B[0] * B[0]));
+    B[2] = pow(product, 1.0 / N);
+    B[3] = N / harmonic_sum;
+    B[4] = max;
+    B[5] = min;
+}
+    
